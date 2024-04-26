934612f26db4fa0b848da565e19eedf249e1a50a
Muhammad Alwaz <167531290+Alwaz12@users.noreply.github.com>
2024-04-26 16:17:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+//Import the inquirer
+import inquirer from "inquirer";
+// Decleare a constant answer
+const answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to coount the word: "
+    }
+]);
+const words = answer.sentence.trim().split(" ");
+//Print the array[]
+console.log(words);
+// print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
